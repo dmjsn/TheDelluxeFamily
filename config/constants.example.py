e57a89717d19f1e106b8c44f843fd25749fd25e6
d_json <d.kucherenko@owlagency.ru>
2025-06-05 20:51:10 +0300

    Добавил в игнор константы

---
+++ config/constants.example.py
@@ -0,0 +1,14 @@
+from typing import Dict
+
+class Constants:
+    INFO_CHANNEL_ID = None
+
+    CHANNEL_IDS: Dict[str, int] = {
+        'welcome_channel_id': None,
+    }
+
+    ROLES_IDS: Dict[str, int] = {
+        'player_role': None,
+        'guest_role': None,
+        'contractor_role': None,
+    }
